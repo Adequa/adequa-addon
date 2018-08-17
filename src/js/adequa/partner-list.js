55599d9e05e2861dc8a5fa15e1bf091a2c6b0123
Itega <julien.zolli1@gmail.com>
2018-08-17 14:51:44 +0200

    fetch partners from server

--- src/js/adequa/partner-list.js
+++ src/js/adequa/partner-list.js
@@ -11,16 +11,32 @@ var adequaPartnerList = (function(){
     };
 
     var refreshList = function(callback){
-        //TODO fetch list from server
-        var raw = `
-        `;
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if (this.status === 200) {
+                    var list = JSON.parse(this.responseText);
+                    vAPI.storage.set({'partnerList': {list, timestamp: Date.now()}});
+                    µBlock.partnerList = list;
 
-        var list = listParser(raw) || [];
-        vAPI.storage.set({'partnerList': {list, timestamp: Date.now()}});
-        µBlock.partnerList = list;
+                    if(typeof callback === "function")
+                        callback(list)
+                }
+            }
+        };
+        req.open('get', 'https://admin-equa.com/api/partners');
+        req.send(null);
 
-        if(typeof callback === "function")
-            callback(list)
+        // var raw = `
+        // lemonde.fr
+        // `;
+        //
+        // var list = listParser(raw) || [];
+        // vAPI.storage.set({'partnerList': {list, timestamp: Date.now()}});
+        // µBlock.partnerList = list;
+        //
+        // if(typeof callback === "function")
+        //     callback(list)
     };
 
     var getPartnerList = function(callback){

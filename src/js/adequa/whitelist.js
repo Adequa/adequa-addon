23ea1d1527864cacba17b7789b0f1e479d0e3090
Unknown <contact@lecoeurd.fr>
2018-08-22 18:49:17 +0200

    refactor($whitelist): rewrite ajax call

--- src/js/adequa/whitelist.js
+++ src/js/adequa/whitelist.js
@@ -1,23 +1,37 @@
 'use strict';
 
+/* TODO stop overwrite actual whitelist and append Adequa's whitelist */
+
+const ajax = function (method, url, body = null) {
+  return new Promise(function (resolve, reject) {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        if (this.status === 200)
+          resolve(this);
+        else
+          reject(this);
+      }
+    };
+
+    req.open(method, url);
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    req.send(body);
+  });
+};
+
 const requestWhitelist = function () {
 
-  const req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
-    if(this.readyState === XMLHttpRequest.DONE) {
-      if(this.status === 200) {
-        let whitelist = '';
-        JSON.parse(this.responseText).forEach(function (url) {
-          whitelist += url.url + '\n';
-        });
+  ajax('get', 'https://admin-equa.com/api/whitelist-urls')
+    .then(function (data) {
+      let whitelist = '';
+      JSON.parse(data.responseText).forEach(function (url) {
+        whitelist += url.url + '\n';
+      });
 
-        µBlock.netWhitelist = µBlock.whitelistFromString(whitelist);
-        µBlock.saveWhitelist();
-      }
-    }
-  };
-  req.open('get', 'https://admin-equa.com/api/whitelist-urls');
-  req.send(null);
+      µBlock.netWhitelist = µBlock.whitelistFromString(whitelist);
+      µBlock.saveWhitelist();
+    });
 
 };
 
